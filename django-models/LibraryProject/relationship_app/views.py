3b61fd92ffdc5389ccbe013667f0703cfde34760
BMwendwaM <mwendwa600@gmail.com>
2025-11-09 21:26:54 +0300

    Add parentheses to the function.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from .models import Book
 from .models import Library
 
 # registration 
+
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -41,7 +42,7 @@ class LibraryDetailView(DetailView):
 # class based view for registration
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
     
