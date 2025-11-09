9c94fecb370c7069fb27551592ae6cab5da35d31
BMwendwaM <mwendwa600@gmail.com>
2025-11-09 18:42:12 +0300

    Correct import, list_books variable name.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path("books/", views.book_list, name="book_list"),
-    path("library/", views.LibraryDetailView.as_view(), name="librarydetail"),
+    path("books/", list_books, name="book_list"),
+    path("library/", LibraryDetailView.as_view(), name="librarydetail"),
 ]
