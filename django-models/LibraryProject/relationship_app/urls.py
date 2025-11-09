8c378d2c57848aa084aff25dcf3ac1cced3bb99d
BMwendwaM <mwendwa600@gmail.com>
2025-11-09 20:45:26 +0300

    Correct import statement in urls.py.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,13 @@
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import list_books, LibraryDetailView, RegisterView
-
-# login
-
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    path("books/", list_books, name="book_list"),
-    path("library/", LibraryDetailView.as_view(), name="librarydetail"),
+    path("books/", views.list_books, name="book_list"),
+    path("library/", views.LibraryDetailView.as_view(), name="librarydetail"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
