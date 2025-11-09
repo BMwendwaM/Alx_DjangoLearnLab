ab6a2c1a12ec7d902ecc8f3124b9c8ba5aba5e07
BMwendwaM <mwendwa600@gmail.com>
2025-11-09 23:07:51 +0300

    Create custom permissions.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -22,6 +22,14 @@ class Book(models.Model):
         return f"{self.title} by {self.author}"
     
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required
 #books
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
@@ -77,3 +77,19 @@ def librarian_view(request):
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
+
+
+# PERMISSION VIEWS
+
+@permission_required("relationship_app.can_add_book")
+def add_book(request):
+   if request.method == 'POST':
+      return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request):
+   return render(request, 'relationship_app/edit_book.html')
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request):
+   return render(request, 'relationship_app/delete_book.html')
