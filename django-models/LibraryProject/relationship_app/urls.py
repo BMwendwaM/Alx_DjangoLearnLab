8921052fae2996c5d89b9e084a717e29a5d6c33f
BMwendwaM <mwendwa600@gmail.com>
2025-11-09 23:12:53 +0300

    Correct path name.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/', views.edit_book, name='edit_book'),
-    path('books/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
 ]
