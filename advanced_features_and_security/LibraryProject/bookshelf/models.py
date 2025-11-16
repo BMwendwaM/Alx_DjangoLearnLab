59f38357af4559357cc328ce430651dba72bbca5
BMwendwaM <mwendwa600@gmail.com>
2025-11-16 12:14:41 +0300

    Fix customusermanager function.

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,20 +1,38 @@
-from django.contrib.auth.models import User, AbstractUser, UserManager
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
 
 
-# CustomeUser
+# CustomeUserManager
 
-class CustomUserManager(UserManager):
-    pass
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The username must be set")
+        extra_fields.setdefault('date_of_birth', None)
+        extra_fields.setdefault('profile_photo', None)
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username=username, password=password, **extra_fields)
+
+# CustomeUser
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(null=True, blank=True)
+    
+    objects = CustomUserManager()
+
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = [] 
+
 
 # Create book class.
 
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -57,4 +57,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user} - {self.role}"
-    
