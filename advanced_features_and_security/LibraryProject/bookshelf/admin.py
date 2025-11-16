ae037da34d200f8cabf06ffe084092fae9ccbbfb
BMwendwaM <mwendwa600@gmail.com>
2025-11-16 12:20:39 +0300

    Fix CustomUserAdmin function in admin.py.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,24 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .models import Book
 
 # Register Book model and custom admin panel
-
-from django.contrib import admin
-from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
-# Register model + custom admin class
-
 admin.site.register(Book, BookAdmin)
 
 
+# Customuser admin
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Information", {'fields': ('date_of_birth', 'profile_photo')}),
@@ -26,3 +22,5 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional Information", {'fields': ('date_of_birth', 'profile_photo')}),
     )
+
+admin.site.register(CustomUser, CustomUserAdmin)
