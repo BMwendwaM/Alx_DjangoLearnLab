2403c42ffb9b5866dde91120a8460d299fb2e318
BMwendwaM <mwendwa600@gmail.com>
2025-11-16 21:57:21 +0300

    Fix function in forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
