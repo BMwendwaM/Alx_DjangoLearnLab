23fad255ed25623b972a092d82e34b309c94b03f
BMwendwaM <mwendwa600@gmail.com>
2025-11-16 11:50:26 +0300

    Fix CustomUser in Bookshelf app.

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -58,8 +58,3 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user} - {self.role}"
     
-
-@receiver(post_save, sender=CustomUser)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
