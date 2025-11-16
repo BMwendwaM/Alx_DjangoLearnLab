7cf2bc5485fe6b2928503050d9a900e01fe77e62
BMwendwaM <mwendwa600@gmail.com>
2025-11-16 22:02:29 +0300

    Fix import statement in .html file.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
+
 
 # CREATE BOOK
 
