ead21bf75425a24ec7d4b94789d8bc940c4d8df0
BMwendwaM <mwendwa600@gmail.com>
2025-12-13 16:24:44 +0300

    Fix post variable in posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -52,7 +52,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
@@ -71,7 +71,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         
         # Check if like exists
         like = Like.objects.filter(user=request.user, post=post).first()
